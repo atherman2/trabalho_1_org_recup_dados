15774b619f77895030736b1f32f425f8dfe36e2c
Alexandre Herman <athermandev@gmail.com>
2024-07-11 11:42:05 -0300

    criei led.py
    adicionei remoção do primeiro elemento da led, com o retorno do registro logicamente removido

---
+++ aplicacao/utilidades/led.py
@@ -0,0 +1,30 @@
+import os, io
+
+class Membro_led:
+    def __init__(self) -> None:
+        self.byte_offset:int = 0
+        self.tam:int = 0
+        self.prox:int = 0
+
+def remove_primeiro_da_led(arq: io.BufferedRandom) -> Membro_led:
+    byte_offset_inicial = arq.tell()
+    arq.seek(0, os.SEEK_SET)
+
+    b_o_primeiro_regis = int.from_bytes(arq.read(4))
+    
+    antigo_primeiro = Membro_led()
+    antigo_primeiro.byte_offset = b_o_primeiro_regis
+    
+    arq.seek(b_o_primeiro_regis, os.SEEK_SET)
+    antigo_primeiro.tam = int.from_bytes(arq.read(2))
+
+    arq.seek(1, os.SEEK_CUR)
+    prox = int.from_bytes(arq.read(4))
+    antigo_primeiro.prox = prox
+
+    arq.seek(0, os.SEEK_SET)
+    bin_prox = prox.to_bytes(4)
+    arq.write(bin_prox)
+    
+    arq.seek(byte_offset_inicial, os.SEEK_SET)
+    return antigo_primeiro
--- aplicacao/utilidades/tem_registro.py
+++ aplicacao/utilidades/tem_registro.py
@@ -1,7 +1,11 @@
 import os, io
 def tem_registro(arq:io.BufferedReader):
     byte_offset_inicial = arq.tell()
-    lido = arq.read(1).decode()
+    bin_lido = arq.read(1)
+    if ord(bin_lido) == 255:
+        lido = '1'
+    else:
+        lido = bin_lido.decode()
     #lido:bytearray = arq.read(3)
     #lido = lido[2].decode()
     arq.seek(byte_offset_inicial, os.SEEK_SET)
