64eeaf614f71145733e9e5dd572146a0ae5c08f6
Alexandre Herman <athermandev@gmail.com>
2024-07-12 08:49:07 -0300

    atualizei o init de utilidades

--- aplicacao/utilidades/__init__.py
+++ aplicacao/utilidades/__init__.py
@@ -2,3 +2,7 @@ from utilidades.le_chave import le_chave
 from utilidades.tem_registro import tem_registro
 from utilidades.byte_offset_pelo_id import byte_offset
 from utilidades.registro import le_registro
+from utilidades.led import Membro_led
+from utilidades.led import acessa_prox_led
+from utilidades.led import tam_regis_da_led
+from utilidades.led import remove_da_led
