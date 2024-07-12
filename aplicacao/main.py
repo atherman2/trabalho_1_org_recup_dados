854191d2bab2dec52152cb9f6b6abbf4a02a80c6
Alexandre Herman <athermandev@gmail.com>
2024-07-12 14:51:17 -0300

    incógnita
    acho que o problema é que o readline() estálendo quebra de linha também

--- aplicacao/main.py
+++ aplicacao/main.py
@@ -10,19 +10,22 @@ import utilidades as u
 #u.testa_dat()
 if __name__ == '__main__':
     arq = open('./aplicacao/dados2.dat', 'rb')
-    f.busca(arq, '22')
+    #f.busca(arq, '22')
     arq.close()
-    '''if sys.argv[1] == '-e':
+    if sys.argv[1] == '-e':
         arq_operacoes: io.BufferedReader = open(sys.argv[2], 'rt')
         while (leitura := arq_operacoes.readline()):
             if leitura[0] == 'b':
                 arq: io.BufferedReader = open('./aplicacao/dados2.dat', 'rb')
+                print(type('22'))
+                print(leitura[2:])
+                print(type(leitura[2:]))
                 f.busca(arq, leitura[2:])
                 print()
                 arq.close()
-            if leitura[0] == 'i':
+            elif leitura[0] == 'i':
                 arq: io.BufferedRandom = open('./aplicacao/dados2.dat', 'r+b')
                 f.insere_registro(arq, leitura[2:])
                 print('/')
-'''
+
 #f.insere_registro(open('./aplicacao/dados2.dat', 'r+b'), '1|2|3|4|5|6|')
